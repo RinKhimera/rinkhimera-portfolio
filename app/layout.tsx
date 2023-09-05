64a0d58d81d60b12618db190001eb615c8647d9d
RinKhimera <dixiades@gmail.com>
2023-09-05 03:23:52 +0100

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+// import { Inter } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+// const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
