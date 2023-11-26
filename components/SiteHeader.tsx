d080415bca321517ad4f544c66f35b3810e39933
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-11-27 00:17:11 +0100

    header links

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,6 +6,7 @@ import AvatarLogo from "@/public/assets/images/avatar.webp"
 import { Github, Linkedin } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { FaLinkedin } from "react-icons/fa"
 import { ModeToggle } from "./theme-toggle"
 
 const navigationLinks = [
@@ -46,15 +47,19 @@ const SiteHeader = () => {
         <div className="flex items-center">
           <Link
             className={buttonVariants({ variant: "ghost", size: "icon" })}
-            href={"/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/RinKhimera"
           >
             <Github />
           </Link>
           <Link
             className={buttonVariants({ variant: "ghost", size: "icon" })}
-            href={"/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/samuel-pokam/"
           >
-            <Linkedin />
+            <FaLinkedin size={24} />
           </Link>
           <ModeToggle />
         </div>
