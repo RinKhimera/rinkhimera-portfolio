214717688f75bde8a82c2b0e8373ac2dd73b4d3f
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-12-18 04:19:24 +0100

    next-contentlayer

--- app/blog/[articleId]/page.tsx
+++ app/blog/[articleId]/page.tsx
@@ -1,10 +1,39 @@
-const Article = async ({ params }: { params: { articleId: string } }) => {
+import { Mdx } from "@/components/mdx-components"
+import { allPosts } from "contentlayer/generated"
+import { format, parseISO } from "date-fns"
+import { useMDXComponent } from "next-contentlayer/hooks"
+import { notFound } from "next/navigation"
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post._raw.flattenedPath,
+  }))
+}
+
+const Article = ({ params }: { params: { articleId: string } }) => {
+  // Find the post for the current page.
+  const post = allPosts.find(
+    (post) => post._raw.flattenedPath === params.articleId,
+  )
+
+  // 404 if the post does not exist.
+  if (!post) notFound()
+
+  // Parse the MDX file via the useMDXComponent hook.
+  const MDXContent = useMDXComponent(post.body.code)
   return (
-    <section className="py-24">
-      <div className="prose dark:prose-invert lg:prose-xl container">
-        My post : {params.articleId}
+    <article className="mx-auto max-w-2xl py-8">
+      <div className="mb-8">
+        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        <h1>{post.title}</h1>
+        {/* <Mdx code={post.body.code} /> */}
+        <div className="prose dark:prose-invert lg:prose-lg">
+          <MDXContent />
+        </div>
       </div>
-    </section>
+    </article>
   )
 }
 
---
+++ contentlayer.config.js
@@ -0,0 +1,82 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import remarkGfm from "remark-gfm"
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+    author: {
+      type: "string",
+      default: "Samuel Pokam",
+      required: true,
+    },
+    // image: {
+    //   type: "string",
+    //   required: true,
+    // },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted")
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"]
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
+})
